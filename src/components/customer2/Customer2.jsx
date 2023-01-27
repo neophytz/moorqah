5a507f206a74f6ec0fbf288cfefbf5ab4f0e863f
NIKHIL GUPTA <nikhilgupta8800@gmail.com>
2023-01-28 00:50:02 +0530

    customer2 component updated

--- src/components/customer2/Customer2.jsx
+++ src/components/customer2/Customer2.jsx
@@ -1,37 +1,28 @@
-/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react'
-import "./customer2.css"
 import canvas from "./Canvas.png"
 
 const Customer2 = () => {
     return (
-        <>
-            <div className='lefttt'>   
-                <div className='columnnn'><div className='firsttt'>
-                    <div className="captionnn">CUSTOMER STORY</div>
-                    <h2>Creating consistency with Canvas.
-                    </h2>
-                    <div className="block-introoo"><p>Virginia Community College System provides flexible pathways in higher education to strengthen individuals and communities. With Canvas as a partner, this rapidly growing system with 23 individual colleges creates consistency in teaching and learning and invests in student success.</p>
-                        <p className='a'>A set of Canvas best practices helps maintain LMS-usage consistency across the colleges. <br />Ubiquitous Canvas use allowed VCCS to transition smoothly to online learning during COVID-19.</p>
+        <div className='container'>
+            <div className='row d-flex align-items-center'>
+                <div className='col-md-6 col-sm-12 mb-4'>
+                    <div className="mb-2"><strong>CUSTOMER STORY </strong> </div>
+                    <h2 className='display-4 mb-2' style={{ fontWeight: "400" }}><strong> Creating consistency with Canvas.</strong></h2>
+                    <p>Virginia Community College System provides flexible pathways in higher education to strengthen individuals and communities. With Canvas as a partner, this rapidly growing system with 23 individual colleges creates consistency in teaching and learning and invests in student success.</p>
+                    <ul style={{ fontWeight: 'bolder' }}>
+                        <li>A set of Canvas best practices helps maintain LMS-usage consistency across the colleges.</li>
+                        <li>Ubiquitous Canvas use allowed VCCS to transition smoothly to online learning during COVID-19.</li>
+                    </ul>
+                    <div className='btn btn-outline-dark px-4 rounded-pill'>
+                        Read the story
                     </div>
-                    <div >
-                        <a className="button-blockkk" href="/" ><span>Read the story</span><i className="fa-solid fa-arrow-right"></i></a>
-                    </div>
-                </div></div>
-                <div className="columnnn">
-                <div className="columnnnn">
-                    <div className='immgg'> <img src={canvas} alt="" /></div>
-                    </div></div>
                 </div>
-      
-
-
-
-                 
-        
-  
-  </>
-  )
+                <div className='col-md-6 col-sm-12'>
+                    <img src={canvas} alt="" />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Customer2
