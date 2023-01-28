c68acb3de1e5ef6651c4d48d4e61afb31b9579a4
NIKHIL GUPTA <nikhilgupta8800@gmail.com>
2023-01-28 07:27:00 +0530

    feature display component updated

--- src/components/FeatureDisplay/FeatureDisplay.js
+++ src/components/FeatureDisplay/FeatureDisplay.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { animated, useSpring } from "react-spring";
-import "./FeatureDisplay.css";
 import cart from "../../assets/FeatureDisplay/cart.svg";
-import arrow from "../../assets/FeatureDisplay/arrow.svg";
 import image1 from "../../assets/FeatureDisplay/1.png";
 import image2 from "../../assets/FeatureDisplay/2.png";
 import image3 from "../../assets/FeatureDisplay/3.png";
@@ -20,105 +18,65 @@ const FeatureDisplay = () => {
 		{
 			title: "SaaS Application Search",
 			image: image2,
-			description:
-				"Decrease your application’s time to market by building the search and discovery experience with Algolia’s APIs.",
+			description:"Decrease your application’s time to market by building the search and discovery experience with Algolia’s APIs.",
 			icon: cart,
 		},
 		{
 			title: "Site Search",
 			image: image3,
-			description:
-				"Increase web visitor conversions by enabling search and discovery across different types of website content.",
+			description:"Increase web visitor conversions by enabling search and discovery across different types of website content.",
 			icon: cart,
 		},
 		{
 			title: "Content Discovery",
 			image: image4,
-			description:
-				"Boost ad or subscription revenue by enabling search and discovery for digital media.",
+			description:"Boost ad or subscription revenue by enabling search and discovery for digital media.",
 			icon: cart,
 		},
 		{
 			title: "Enterprise & Workplace Search",
 			image: image5,
-			description:
-				"Enable enterprise-grade customer search and discovery, while empowering employees to find information across company systems.",
+			description:"Enable enterprise-grade customer search and discovery, while empowering employees to find information across company systems.",
 			icon: cart,
 		},
 	];
 	const [myProfession, setMyProfession] = useState("Product Discovery");
 
 	return (
-		<div className="second-page">
-			{/* <div className="heading-text">
-        <h1>Powerful, flexible, scalable digital experiences for your business</h1>
-      </div> */}
-			<div>
-				{professions.map((profession, idx) => (
-					<div key={idx} onClick={() => setMyProfession(profession.title)}>
-						<div
-							class="card-btn-selected"
-							style={
-								myProfession === profession.title
-									? {
-											borderRadius: "16px",
-											boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
-											position: "relative",
-											zIndex: "1",
-											borderBottom: "0px solid rgb(2, 0, 0)",
-									  }
-									: {}
-							}
-						>
-							<div
-								className="card-btn-icon"
-								style={
-									myProfession === profession.title
-										? {
-												backgroundColor: "#003dff",
-										  }
-										: {}
-								}
-							>
-								<img className="cart" src={profession.icon} alt="cart-icn" />
-							</div>
-							<div className="card-btn-content">
-								<h1 className="lead-content">
-									{profession.title.toUpperCase()}
-								</h1>
-								{myProfession === profession.title && (
-									<>
-										<p className="support-content">{profession.description}</p>
-										<div className="btn-div">
-											<button className="link-btn">B2C Solutions</button>
-											<img src={arrow} className="arrow" alt="arrow" />
+		<>
+			<div className="container">
+				<div className="text-center mb-4">
+					<h1 className=" display-3"><strong>Powerful, flexible, scalable digital experiences for your business</strong></h1>
+				</div>
+				<div className="row d-flex justify-content-center align-items-center">
+					<div className="col">
+						{professions.map((profession, idx) => (
+							<div key={idx} onClick={() => setMyProfession(profession.title)}>
+								<div className={`p-4 ${myProfession === profession.title && "card shadow"}`}>
+									<div className="d-flex mt-2 align-items-start">
+										<img className={`rounded p-1 ${myProfession === profession.title ? 'bg-primary' : "bg-dark"}`} src={profession.icon} alt="cart-icon" height='30px' />
+										<div className="mx-2">
+											<h4>{profession.title.toUpperCase()}</h4>
+											<div>
+												{myProfession === profession.title && (
+													<>
+														<p>{profession.description}</p>
+														<div className="btn btn-outline-primary px-4">B2C Solutions</div>
+													</>
+												)}
+											</div>
 										</div>
-									</>
-								)}
+									</div>
+								</div>
 							</div>
-						</div>
+						))}
 					</div>
-				))}
+					<div className="col d-flex justify-content-center align-items-center h-100">
+						{professions.map((profession, idx) => <div key={idx}> {myProfession === profession.title && <ProfessionImage src={profession.image} />} </div>)}
+					</div>
+				</div>
 			</div>
-
-			<div className="images">
-				<p>
-					{myProfession === "Product Discovery" && (
-						<ProfessionImage src={image1} />
-					)}
-					{myProfession === "SaaS Application Search" && (
-						<ProfessionImage src={image2} />
-					)}
-					{myProfession === "Site Search" && <ProfessionImage src={image3} />}
-					{myProfession === "Content Discovery" && (
-						<ProfessionImage src={image4} />
-					)}
-					{myProfession === "Enterprise & Workplace Search" && (
-						<ProfessionImage src={image5} />
-					)}
-				</p>
-			</div>
-		</div>
+		</>
 	);
 };
 
