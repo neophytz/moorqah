790facecc12913fd6adf39d4c0d6c32b37c96024
Shobhan-Pakhira <87083191+Shobhan-Pakhira@users.noreply.github.com>
2023-02-01 00:40:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,6 +27,8 @@ function App() {
       <ScrollAnimation/>
       <ScrollCard />
       <CardPage />
+      <CardPage />
+      <CardPage />
 
       {/* <Preload3 /> */}
       {/* <Preload /> */}
